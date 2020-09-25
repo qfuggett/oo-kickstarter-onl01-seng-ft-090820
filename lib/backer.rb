55b398d59602e5d83239db5f9b31dc4a45405d95
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-25 16:40:46 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  attr_reader :backed_projects, :name
+  
+  
+    
+  def initialize(name)
+    @name = name
+    @backed_projects = [ ]
+  end
+  
+  def back_project(name)
+    @backed_projects << name
+    project.backers << self
+  end
+  
+end
